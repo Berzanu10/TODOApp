185b91b2bb6038355c6eccc1af6a2e950221b31c
Berzan Uludağ <berzanu10@gmail.com>
2026-05-09 22:08:55 +0300

    güzel durumda

--- src/Components/TodoItem.tsx
+++ src/Components/TodoItem.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { ITodo } from '../Interfaces/Todo';
+import type { ITodo } from '../Interfaces/Todo';
 
 interface TodoItemProps {
   todo: ITodo;
--- src/Pages/TodoPage.tsx
+++ src/Pages/TodoPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { ITodo } from '../Interfaces/Todo';
+import type { ITodo } from '../Interfaces/Todo';
 import TodoItem from '../Components/TodoItem';
 
 const TodoPage: React.FC = () => {
